ecc8395fe8b189013f9f7ee2f0192c6669b653d4
OraneUtan <Oran9eUtan@gmail.com>
2020-05-27 12:22:40 +0200

    Root items for custom models can now be configured in registry

--- build/generate_base_item_models.py
+++ build/generate_base_item_models.py
@@ -8,33 +8,11 @@ def minecraft_item_model_overwrite(custom_model_data, model_path):
 	""" Creates a custom model overwrite for minecraft item models """
 	return { "predicate": { "custom_model_data": custom_model_data }, "model": model_path }
 
-def minecraft_item_model(base_model, overrides=None):
-	""" Creates a minecraft item model """
-	if overrides:
-		base_model['overrides'] = overrides
-	return base_model
-
 registry = Registry()
 hats = list(registry.all_hats())
 overrides = [minecraft_item_model_overwrite(hat.custom_model_data, hat.model_path) for hat in hats]
 
-# Create item model for hats
-with open("resourcepack/assets/minecraft/models/item/leather_helmet.json", "w+") as file:
-	leather_helmet = minecraft_item_model({
-		"parent": "item/generated",
-		"textures": {
-			"layer0": "item/leather_helmet",
-			"layer1": "item/leather_helmet_overlay"
-		}
-	}, overrides)
-	json.dump(leather_helmet, file, separators=(',', ':'))
-
-# Create item model for hats on entities heads
-with open("resourcepack/assets/minecraft/models/item/stick.json", "w+") as file:
-	stick = minecraft_item_model({
-		"parent": "item/handheld",
-		"textures": {
-			"layer0": "item/stick"
-		}
-	}, overrides)
-	json.dump(stick, file, separators=(',', ':'))
+for key, item_model in registry.overwritten_item_models.items():
+	item_model.model["overrides"] = overrides
+	with open(item_model.path, "w+") as file:
+		json.dump(item_model.model, file, separators=(',', ':'))
--- build/registry.py
+++ build/registry.py
@@ -4,10 +4,12 @@ from dataclasses import dataclass
 class Registry:
 	def __init__(self):
 		self.registry = load_yaml_file("hat_registry.yml")
+		self.overwritten_item_models = Registry._parse_root_items(self.registry)
+		self.categories = Registry._parse_categories(self.registry)
 
 	def all_hats(self):
-		for category_name, category_hats in map(lambda category: list(category.items())[0], self.registry['categories']):
-			for hat in self._parse_hats(category_name, category_hats):
+		for category_name, category_hats in self.categories.items():
+			for hat in category_hats:
 				yield hat
 
 	def categories(self):
@@ -15,7 +17,16 @@ class Registry:
 			hats = list(self._parse_hats(category_name, category_hats))
 			yield (category_name, hats)
 
-	def _parse_hats(self, category_name, json_hats):
+	@classmethod
+	def _parse_categories(cls, registry):
+		categories = dict()
+		for category_name, category_hats in map(lambda category: list(category.items())[0], registry['categories']):
+			hats = list(Registry._parse_hats(category_name, category_hats))
+			categories[category_name] = hats
+		return categories
+
+	@classmethod
+	def _parse_hats(cls, category_name, json_hats):
 		for hat_name, hat_data in map(lambda category_entry: list(category_entry.items())[0], json_hats):
 			custom_model_data = hat_data['custom_model_data']
 			
@@ -30,6 +41,22 @@ class Registry:
 				translation = f"item.hats.{category_name}.{hat_name}"
 			yield Hat(hat_name, category_name, custom_model_data, model, type, translation)
 
+	@classmethod
+	def _parse_root_items(cls, registry):
+		root_items = dict()
+		for key, val in registry["overwritten-item-models"].items():
+			id = val["id"]
+			if id.startswith("minecraft"):
+				id = id[len("minecraft:"):]
+			root_items.update({key:ItemModel(val["id"], f"resourcepack/assets/minecraft/models/item/{id}.json", val["model"])})
+		return root_items
+
+@dataclass
+class ItemModel:
+	id: str
+	path: str
+	model: dict
+		
 @dataclass
 class Hat:
 	name: str
@@ -38,7 +65,6 @@ class Hat:
 	model_path: str
 	type: str
 	translation: str
-
 
 def load_yaml_file(path):
 	with open(path) as file:
