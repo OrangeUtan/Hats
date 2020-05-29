153e2adefac8a68bae9501a1f46959461a1e7bd9
OraneUtan <Oran9eUtan@gmail.com>
2020-05-29 14:21:26 +0200

    Added "ReferenceEntry" class to animation buildscript

--- build/generate_animations.py
+++ build/generate_animations.py
@@ -2,6 +2,7 @@ import os, yaml
 import json as JSON
 from dataclasses import dataclass
 from fractions import Fraction
+from enum import Enum
 
 @dataclass
 class State:
@@ -26,12 +27,14 @@ class Sequence:
 		# Collect some initial information about the entries
 		is_weighted = False
 		total_weight = 0
-		for entry in json:
-			if "weight" in entry:
+		entries = []
+		for entry in map(lambda entry: ReferenceEntry.from_json(entry), json):
+			entries.append(entry)
+			if entry.weight:
 				is_weighted = True
-				total_weight += entry["weight"]
+				total_weight += entry.weight
 
-		return Sequence(name, json, is_weighted, total_weight)
+		return Sequence(name, entries, is_weighted, total_weight)
 
 	def to_frames(self, states, sequences, duration=None):
 		if self.is_weighted:
@@ -43,65 +46,60 @@ class Sequence:
 
 	def _convert_entries_abs_time(self, states, sequences):
 		for entry in self.entries:
-			type, ref, repeat = entry["type"], entry["ref"], max(1,entry.get("repeat", 1))
+			entry.check_is_valid_reference(states, sequences)
 
-			if type == "state":
-				# Get the frame index
-				if not ref in states:
-					raise Exception(f"State '{ref}' doesn't exist")
-				index = states[ref].index
-
-				time = entry.get("duration", 1)
-				yield self._frame(index, time*repeat)
+			if entry.type == ReferenceType.STATE:
+				index = states[entry.ref].index
+				time = entry.duration
+				yield self._frame(index, time*entry.repeat)
 			
-			elif type == "sequence":
-				if not ref in sequences:
-					raise Exception(f"Sequence '{ref}' doesn't exist")
-				referenced_sequence = sequences[ref]
-
-				seq_duration = entry.get("duration", None)
+			elif entry.type == ReferenceType.SEQUENCE:
+				referenced_sequence = sequences[entry.ref]
+				seq_duration = entry.duration
 
 				seq_frames = referenced_sequence.to_frames(states, sequences, seq_duration)
-				for i in range(repeat):
+				for i in range(entry.repeat):
 					for frame in seq_frames:
 						yield frame.copy()
 
 	def _convert_entries_weighted(self, states, sequences, duration):
 		if self.is_weighted and not duration:
-			raise Exception("Parameter 'duration' missing")
+			raise Exception(f"Didn't pass duration to weighted sequence '{self.name}'")
 
 		fixed_duration = self._calc_fixed_duration(sequences)
+		if duration <= fixed_duration:
+			raise Exception(f"Duration passed to weighted sequence {self.name} is smaller than its fixed duration")
+
 		duration -= fixed_duration
 
 		frames = []
 		remaining_duration = duration
-		for entry in self.entries:
-			type, ref, repeat = entry["type"], entry["ref"], max(1,entry.get("repeat", 1))
+		for i, entry in enumerate(self.entries):
+			entry.check_is_valid_reference(states, sequences)
 
-			if type == "state":
-				# Get the frame index
-				if not ref in states:
-					raise Exception(f"State '{ref}' doesn't exist")
-				index = states[ref].index
+			if entry.type == ReferenceType.STATE:
+				index = states[entry.ref].index
 
-				if not "weight" in entry:
-					frames.append(self._frame(index, entry.get("duration", 1)*repeat))
+				if entry.is_weighted(sequences):
+					if remaining_duration <= 0:
+						raise Exception(f"Duration is exhausted. Can't fit {i+1}. entry '{entry.ref}'")
+
+					state_duration = entry.calc_weighted_duration(self.total_weight, duration)
+					state_duration = max(1,min(remaining_duration, state_duration))
+					remaining_duration -= state_duration
+					frames.append(self._frame(index, state_duration))
 				else:
-					time = self._calc_weighted_time(index, entry.get("weight", 1), duration, remaining_duration)
-					remaining_duration -= time
-					frames.append(self._frame(index, time*repeat))
+					frames.append(self._frame(index, entry.duration))
 
-			elif type == "sequence":
-				# Get the referenced sequence
-				if not ref in sequences:
-					raise Exception(f"Sequence '{ref}' doesn't exist")
-				referenced_sequence = sequences[ref]
+			elif entry.type == ReferenceType.SEQUENCE:
+				referenced_sequence = sequences[entry.ref]
 
-				for i in range(repeat):
+				for i in range(entry.repeat):
 					if not referenced_sequence.is_weighted:
 						frames += referenced_sequence.to_frames(states, sequences)
 					else:
-						seq_duration = self._calc_weighted_time(index, entry.get("weight", 1)/repeat, duration, remaining_duration)
+						seq_duration = entry.weighted_duration(total_weight, duration)
+						seq_duration = max(1,min(remaining_duration, seq_duration))
 						remaining_duration -= seq_duration
 						frames += referenced_sequence.to_frames(states, sequences, seq_duration)
 
@@ -116,32 +114,11 @@ class Sequence:
 		return frames
 
 	def _calc_fixed_duration(self, sequences):
-		fixed_duration = 0
-		for entry in self.entries:
-			type, ref, repeat = entry["type"], entry["ref"], max(1,entry.get("repeat", 1))
-
-			if type == "state":
-				if not "weight" in entry:
-					fixed_duration += entry.get("duration", 1)*repeat
-			elif type == "sequence":
-				if not ref in sequences:
-					raise Exception(f"Sequence '{ref}' doesn't exist")
-				referenced_sequence = sequences[ref]
-
-				if referenced_sequence.is_weighted:
-					fixed_duration += referenced_sequence.get("duration", 1)
-				else:
-					fixed_duration += referenced_sequence._calc_fixed_duration(sequences)*repeat
-
-		return fixed_duration
+		return sum(map(lambda entry: entry.calc_fixed_duration(sequences), self.entries))
 
 	@classmethod
 	def _frame(cls, index, time):
 		return {"index": index, "time": time}
-
-	def _calc_weighted_time(self, index, weight, duration, remaining_duration):
-		weighted_time = int((weight*duration)/self.total_weight) # Calculate time based on weight
-		return max(1,min(remaining_duration, weighted_time)) # Assure that: 1 <= time <= remaining_time
 
 	@classmethod
 	def _combine_consecutive_frames(cls, frames):
@@ -159,6 +136,71 @@ class Sequence:
 		if prev_frame:
 			yield prev_frame
 			
+class ReferenceType(Enum):
+	STATE = 1
+	SEQUENCE = 2
+
+	@classmethod
+	def from_string(cls,str):
+		if str == "state":
+			return ReferenceType.STATE
+		elif str == "sequence":
+			return ReferenceType.SEQUENCE
+
+class ReferenceEntry:
+
+	def __init__(self, type, reference, repeat, weight=None, duration=None):
+		self.type = type
+		self.ref = reference
+		self.repeat = repeat
+		self.weight = weight
+		self.duration = duration
+
+	@classmethod 
+	def from_json(cls, json):
+		type = ReferenceType.from_string(json["type"])
+		ref = json["ref"]
+		repeat = max(1, json.get("repeat",1)) # Repeat at least once
+		weight = json.get("weight")
+		duration = json.get("duration")
+
+		return ReferenceEntry(type, ref, repeat, weight, duration)
+
+	def check_is_valid_reference(self, states, sequences):
+		if self.type == ReferenceType.STATE and not self.ref in states:
+			raise Exception(f"State '{self.ref}' doesn't exist")
+		elif self.type == ReferenceType.SEQUENCE and not self.ref in sequences:
+			raise Exception(f"Sequence '{self.ref}' doesn't exist")
+
+	def is_weighted(self, sequences):
+		if self.type == ReferenceType.STATE:
+			return self.weight
+		elif self.type == ReferenceType.SEQUENCE:
+			if self.weight:
+				return True
+			else:
+				return sequences[ref].is_weighted
+
+	def calc_fixed_duration(self, sequences):
+		fixed_duration = 0
+		if self.type == ReferenceType.STATE:
+			if not self.is_weighted(sequences):
+				fixed_duration = self.duration*self.repeat
+		elif self.type == ReferenceType.SEQUENCE:
+			referenced_sequence = sequences[self.ref]
+
+			if referenced_sequence.is_weighted:
+				fixed_duration = referenced_sequence.get("duration", 1)
+			else:
+				fixed_duration = referenced_sequence._calc_fixed_duration(sequences)*self.repeat
+		return fixed_duration
+
+	def calc_weighted_duration(self, total_weight, total_duration):
+		if self.type == ReferenceType.STATE:
+			# Ignore repeat
+			return int((self.weight*total_duration)/total_weight) # Calculate duration based on weight
+		elif self.type == ReferenceType.SEQUENCE:
+			return int(((self.weight/self.repeat)*total_duration)/total_weight) # Calculate duration based on weight
 	
 @dataclass
 class Animation:
