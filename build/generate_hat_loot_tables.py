20073e54444717fa3d1d23b1ea2d4a73f3a1d91e
Oran9eUtan <oran9eutan@gmail.com>
2020-07-14 23:15:44 +0200

    Changed namespace of all hat loot tables

--- build/generate_hat_loot_tables.py
+++ build/generate_hat_loot_tables.py
@@ -41,7 +41,7 @@ def hat_loot_table(hat, item):
 	}
 
 def delete_folder(path: str):
-	for root, dirs, files in os.walk('/path/to/folder'):
+	for root, dirs, files in os.walk(path):
 		for f in files:
 			os.unlink(os.path.join(root, f))
 		for d in dirs:
@@ -49,8 +49,8 @@ def delete_folder(path: str):
 
 registry = Registry()
 
-delete_folder("datapack/data/hats/loot_tables/hat")
-delete_folder("datapack/data/hats/loot_tables/hat_on_head")
+delete_folder(f"{registry.loot_tables_dir}/hat")
+delete_folder(f"{registry.loot_tables_dir}/hat_on_head")
 
 for _, hat in registry.cmd_to_hat_map.items():
 	rel_path = f"{hat.category}/{hat.name}"
@@ -58,16 +58,16 @@ for _, hat in registry.cmd_to_hat_map.items():
 	item_inv = registry.get_item_inv(hat.item_inv)
 	item_on_head = registry.get_item_on_head(hat.item_on_head)
 
-	hat_loot_table_dir = f"datapack/data/hats/loot_tables/hat/{hat.category}"
-	if not os.path.exists(hat_loot_table_dir):
-		os.makedirs(hat_loot_table_dir)
-
 	# Create item inv loot table
-	hat_loot_table_path = f"datapack/data/hats/loot_tables/hat/{rel_path}.json"
+	hat_loot_table_path = f"{registry.loot_tables_dir}/hat/{rel_path}.json"
+	if not os.path.exists(os.path.split(hat_loot_table_path)[0]):
+		os.makedirs(os.path.split(hat_loot_table_path)[0])
 	with open(hat_loot_table_path, "w+") as file:
 		json.dump(hat_loot_table(hat, item_inv), file, separators=(',', ':'), indent=4)
 
 	# Create item on head loot table
-	hat_on_head_loot_table_path = f"datapack/data/hats/loot_tables/hat_on_head/{rel_path}.json"
+	hat_on_head_loot_table_path = f"{registry.loot_tables_dir}/hat_on_head/{rel_path}.json"
+	if not os.path.exists(os.path.split(hat_on_head_loot_table_path)[0]):
+		os.makedirs(os.path.split(hat_on_head_loot_table_path)[0])
 	with open(hat_on_head_loot_table_path, "w+") as file:
 		json.dump(hat_loot_table(hat, item_on_head), file, separators=(',', ':'), indent=4)
--- datapack/data/oran9eutan/functions/hats/dress/mob_as_ghost.mcfunction
+++ datapack/data/oran9eutan/functions/hats/dress/mob_as_ghost.mcfunction
@@ -2,7 +2,7 @@
 # requires: Executor must have ArmorItems nbt and a hat slot
 # descr: Dresses a mob with a ghost hat and gives it invisibility
 
-loot replace entity @s armor.head loot hats:hat_on_head/halloween/wiggly_ghast
+loot replace entity @s armor.head loot oran9eutan:hats/hat_on_head/halloween/wiggly_ghast
 data modify entity @s ArmorDropChances[3] set value 1.0f
 effect give @s minecraft:invisibility 99999 1 true
 tag @s add hats.mob.is_ghost
