e7be8e96561d6c74fbdc71712a31365b605185a3
Oran9eUtan <oran9eutan@gmail.com>
2021-03-01 11:02:38 +0100

    Added build commands

--- generator/cli.py
+++ generator/cli.py
@@ -2,6 +2,8 @@ import json
 from pathlib import Path
 import typer
 import itertools
+import shutil
+import os
 
 from generator.registry import Registry
 from generator import utils
@@ -12,6 +14,36 @@ cmd_app = typer.Typer(short_help="Perform Custom Model Data queries")
 gen_app = typer.Typer(short_help="Generate files")
 app.add_typer(cmd_app, name="cmd")
 app.add_typer(gen_app, name="gen")
+
+
+@app.command(name="build-datapack")
+def cli_build_datapack():
+    build_dir = Path("build")
+    print(build_dir.absolute())
+
+    for dir_name, _, files in os.walk(Path("datapack")):
+        dst_dir = Path(build_dir, dir_name)
+        dst_dir.mkdir(parents=True, exist_ok=True)
+
+        for file in files:
+            src_path = Path(dir_name, file)
+            dst_path = Path(dst_dir, file)
+            shutil.copy(src_path, dst_path)
+
+
+@app.command(name="build-resourcepack")
+def cli_build_resourcepack():
+    build_dir = Path("build")
+    print(build_dir.absolute())
+
+    for dir_name, _, files in os.walk(Path("resourcepack")):
+        dst_dir = Path(build_dir, dir_name)
+        dst_dir.mkdir(parents=True, exist_ok=True)
+
+        for file in files:
+            src_path = Path(dir_name, file)
+            dst_path = Path(dst_dir, file)
+            shutil.copy(src_path, dst_path)
 
 
 @gen_app.command(name="loot-tables", short_help="Generate all hat loot tables")
