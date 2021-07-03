3f90a82f00a4495b6a4f3675a957eb28ebe8dfac
Oran9eUtan <oran9eutan@gmail.com>
2021-07-04 01:18:54 +0200

    chore: Hat showcases for README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Hats
 Hats datapack for minecraft
+
+<div>
+	<img src="media/showcase_accessories.gif" width="24%"/>
+	<img src="media/showcase_animals.gif" width="24%"/>
+	<img src="media/showcase_cats.gif" width="24%"/>
+	<img src="media/showcase_glasses.gif" width="24%"/>
+	<img src="media/showcase_halloween.gif" width="24%"/>
+	<img src="media/showcase_mario.gif" width="24%"/>
+	<img src="media/showcase_misc.gif" width="24%"/>
+</div>
---
+++ hats/plugins/devtools.py
@@ -0,0 +1,37 @@
+from logging import getLogger
+
+from beet import Context
+from beet.library.data_pack import DataPack, Function
+
+from hats.registry.hats import Hat, HatRegistry
+
+logger = getLogger(__name__)
+
+
+def beet_default(ctx: Context):
+    namespace = ctx.meta["namespace"]
+    config = ctx.meta["hats"]
+    cmd_id = int(config["cmd_id"])
+
+    registry = HatRegistry.get(cmd_id)
+
+    ctx.data.merge(_generate_category_fashion_show_functions(namespace, registry))
+
+
+def _generate_category_fashion_show_functions(namespace: str, registry: HatRegistry):
+    data = DataPack()
+
+    for category, hats in registry.categories.items():
+        lines = [
+            f"execute if score #show_progress hats.math matches {i} run loot replace entity @e[tag=fashion_model] armor.head loot {namespace}/hat_on_head/{hat.type}"
+            for i, hat in enumerate(hats)
+        ]
+
+        lines += [
+            "scoreboard players add #show_progress hats.math 1",
+            f"execute if score #show_progress hats.math matches {len(hats)}.. run scoreboard players set #show_progress hats.math 0",
+        ]
+
+        data.functions[f"{namespace}/dev/fashion_show_{category}"] = Function("\n".join(lines))
+
+    return data
