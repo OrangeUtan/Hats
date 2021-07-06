063903555b873cd145e6beea1370716cedb88476
Oran9eUtan <oran9eutan@gmail.com>
2021-07-06 10:44:41 +0200

    refactor: Remove template cache
    Using the Jinja environment supplied by the beet context. No longer caching templated files as they are already cached by Jinja.

--- hats/plugins/hat_loot_tables.py
+++ hats/plugins/hat_loot_tables.py
@@ -1,9 +1,6 @@
-import json
 from logging import getLogger
 
-import jinja2
-from beet import Context, LootTable
-from beet.library.data_pack import DataPack
+from beet import Context, DataPack, LootTable
 
 from hats.registry.hats import Hat, HatRegistry
 
@@ -15,69 +12,65 @@ def beet_default(ctx: Context):
     config = ctx.meta["hats"]
     cmd_id = int(config["cmd_id"])
 
-    cache = ctx.cache["hats"]
-    if cache.has_changed(HatRegistry.PATH, "hats/plugins/hat_loot_tables.py"):
-        logger.info(f"Generating hat loot tables")
-        data = _create_loot_tables(namespace, cmd_id)
-        data.save(path=cache.get_path("loot_tables"), overwrite=True)
-    else:
-        logger.info("Using cached hat loot tables")
-        data = DataPack(path=cache.get_path("loot_tables"))
-
-    ctx.data.merge(data)
+    ctx.data.merge(_create_loot_tables(ctx, namespace, cmd_id))
 
 
-def _create_loot_tables(namespace: str, cmd_id: int):
+def _create_loot_tables(ctx: Context, namespace: str, cmd_id: int):
     data = DataPack()
 
     registry = HatRegistry.get(cmd_id)
-
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("hats/templates"))
 
     for category, hats in registry.categories.items():
         # Create category loot tables
         for model_type in ["hat", "hat_on_head"]:
             data.loot_tables[
                 f"{namespace}/{model_type}/{category}/all"
-            ] = _create_all_hats_from_collection_loot_table(env, hats, namespace, model_type)
+            ] = _create_all_hats_from_collection_loot_table(ctx, hats, namespace, model_type)
             data.loot_tables[
                 f"{namespace}/{model_type}/{category}/random"
-            ] = _create_random_hat_from_collection_loot_table(env, hats, namespace, model_type)
+            ] = _create_random_hat_from_collection_loot_table(ctx, hats, namespace, model_type)
 
         # Create hat loot tables
         for hat in hats:
             data.loot_tables[f"{namespace}/hat_on_head/{hat.type}"] = _create_hat_loot_table(
-                env, hat, hat.model_head
+                ctx, hat, hat.model_head
             )
             data.loot_tables[f"{namespace}/hat/{hat.type}"] = _create_hat_loot_table(
-                env, hat, hat.model_inventory
+                ctx, hat, hat.model_inventory
             )
 
     return data
 
 
-def _create_hat_loot_table(env: jinja2.Environment, hat: Hat, item_model_id: str):
-    template = env.get_template("loot_tables/hat.json")
-    rendered = template.render(
-        nbt_tag={"CustomModelData": hat.cmd, "Tags": ["hats.hat", hat.type_tag]},
-        localized_name=hat.localized_name,
-        item_model_id=item_model_id,
-        localized_lore=hat.localized_lore,
+def _create_hat_loot_table(ctx: Context, hat: Hat, item_model_id: str):
+    return LootTable(
+        ctx.template.render(
+            "loot_tables/hat.json",
+            nbt_tag={"CustomModelData": hat.cmd, "Tags": ["hats.hat", hat.type_tag]},
+            localized_name=hat.localized_name,
+            item_model_id=item_model_id,
+            localized_lore=hat.localized_lore,
+        )
     )
-    return LootTable(json.loads(rendered))
 
 
 def _create_all_hats_from_collection_loot_table(
-    env: jinja2.Environment, hats: list[Hat], namespace: str, model_type: str
+    ctx: Context, hats: list[Hat], namespace: str, model_type: str
 ):
-    template = env.get_template("loot_tables/all_from_collection.json")
-    rendered = template.render(loot_tables=[f"{namespace}/{model_type}/{hat.type}" for hat in hats])
-    return LootTable(json.loads(rendered))
+    return LootTable(
+        ctx.template.render(
+            "loot_tables/all_from_collection.json",
+            loot_tables=[f"{namespace}/{model_type}/{hat.type}" for hat in hats],
+        )
+    )
 
 
 def _create_random_hat_from_collection_loot_table(
-    env: jinja2.Environment, hats: list[Hat], namespace: str, model_type: str
+    ctx: Context, hats: list[Hat], namespace: str, model_type: str
 ):
-    template = env.get_template("loot_tables/random_from_collection.json")
-    rendered = template.render(loot_tables=[f"{namespace}/{model_type}/{hat.type}" for hat in hats])
-    return LootTable(json.loads(rendered))
+    return LootTable(
+        ctx.template.render(
+            "loot_tables/random_from_collection.json",
+            loot_tables=[f"{namespace}/{model_type}/{hat.type}" for hat in hats],
+        )
+    )
--- hats/plugins/item_models.py
+++ hats/plugins/item_models.py
@@ -1,16 +1,11 @@
-import json
 from dataclasses import dataclass
 from logging import getLogger
 
-import jinja2
-from beet import Context
-from beet.library.resource_pack import Model, ResourcePack
+from beet import Context, Model, ResourcePack
 
 from hats.registry.hats import HatRegistry
 
 logger = getLogger(__name__)
-
-CACHE_KEY = "item_models"
 
 
 @dataclass
@@ -23,24 +18,15 @@ def beet_default(ctx: Context):
     config = ctx.meta["hats"]
     cmd_id = int(config["cmd_id"])
 
-    cache = ctx.cache["hats"]
-    if cache.has_changed(HatRegistry.PATH, "hats/plugins/item_models.py"):
-        logger.info(f"Generating item models")
-        assets = _create_item_models(cmd_id)
-        assets.save(path=cache.get_path(CACHE_KEY), overwrite=True)
-    else:
-        logger.info("Using cached item models")
-        assets = ResourcePack(path=cache.get_path(CACHE_KEY))
-
-    ctx.assets.merge(assets)
+    ctx.assets.merge(item_models(ctx, cmd_id))
 
 
-def _create_item_models(cmd_id: int):
+def item_models(ctx: Context, cmd_id: int):
+    """Create item models used for custom model data"""
+
     assets = ResourcePack()
 
     registry = HatRegistry.get(cmd_id)
-
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader("hats/templates"))
 
     overrides = []
     for category, hats in registry.categories.items():
@@ -48,10 +34,10 @@ def _create_item_models(cmd_id: int):
     overrides = sorted(overrides, key=lambda o: o.cmd)
 
     assets.models["minecraft:item/stick"] = Model(
-        json.loads(env.get_template("models/stick.json").render(overrides=overrides))
+        ctx.template.render("models/stick.json", overrides=overrides)
     )
     assets.models["minecraft:item/leather_helmet"] = Model(
-        json.loads(env.get_template("models/leather_helmet.json").render(overrides=overrides))
+        ctx.template.render("models/leather_helmet.json", overrides=overrides)
     )
 
     return assets
