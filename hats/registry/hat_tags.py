6bf580a37f75ba69a184dc9f1691d0f42152f2c2
Oran9eUtan <oran9eutan@gmail.com>
2021-07-10 18:34:34 +0200

    refactor: Rename hat tags

--- hats/registry/hat_tags.py
+++ hats/registry/hat_tags.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 import yaml
@@ -9,6 +10,17 @@ class HatTagRegistry(dict):
     PATH = Path("hats/registry/hat_tags.yml")
 
     @classmethod
-    def get(cls) -> dict[str, list[str]]:
+    def from_json(cls, json: dict):
+        registry: dict[str, list[str]] = defaultdict(list)
+        for tag, hat_types in json.items():
+            for hat_type in hat_types:
+                if hat_type[0] == "#":
+                    registry[tag] += registry[hat_type[1:]]
+                else:
+                    registry[tag].append(hat_type)
+        return registry
+
+    @classmethod
+    def get(cls):
         with cls.PATH.open("r") as f:
-            return yaml.safe_load(f)
+            return cls.from_json(yaml.safe_load(f))
